81a7e72b76c241393373cc93937ec42be4339e77
bahrmichael <mbahr92@gmail.com>
2017-09-26 13:23:13 +0200

    tests

--- src/main/java/com/buyback/eve/service/KillmailParser.java
+++ src/main/java/com/buyback/eve/service/KillmailParser.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import com.buyback.eve.domain.Killmail;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,32 +24,39 @@ public class KillmailParser {
             log.error("JSONObject for characterID {} was null. Skipping.");
             throw new IllegalArgumentException("KillmailParser#parseKillmail JSONObject was null.");
         }
-        Killmail result = new Killmail();
-        result.setKillId(object.getLong("killID"));
-        result.setSolarSystemId(object.getLong("solarSystemID"));
-        result.setKillTime(object.getString("killTime"));
-        JSONArray attackers = object.getJSONArray("attackers");
-        for (int i = 0; i < attackers.length(); i++) {
-            long attackerId = attackers.getJSONObject(i).getLong("characterID");
-            result.addAttackerId(attackerId);
-            int finalBlow = attackers.getJSONObject(i).getInt("finalBlow");
-            if (finalBlow == 1) {
-                result.setFinalBlowAttackerId(attackerId);
+        try {
+            Killmail result = new Killmail();
+            result.setKillId(object.getLong("killID"));
+            result.setSolarSystemId(object.getLong("solarSystemID"));
+            result.setKillTime(object.getString("killTime"));
+            JSONArray attackers = object.getJSONArray("attackers");
+            for (int i = 0; i < attackers.length(); i++) {
+                long attackerId = attackers.getJSONObject(i).getLong("characterID");
+                result.addAttackerId(attackerId);
+                int finalBlow = attackers.getJSONObject(i).getInt("finalBlow");
+                if (finalBlow == 1) {
+                    result.setFinalBlowAttackerId(attackerId);
+                }
             }
+            result.setNpc(object.getJSONObject("zkb").getBoolean("npc"));
+            result.setTotalValue(object.getJSONObject("zkb").getLong("totalValue"));
+            result.setPoints(object.getJSONObject("zkb").getLong("points"));
+            result.setVictimId(object.getJSONObject("victim").getLong("characterID"));
+            result.setVictimAlliance(object.getJSONObject("victim").getString("allianceName"));
+            return result;
+        } catch (JSONException jsonException) {
+            log.warn("A Killmail could not be parsed.", jsonException);
+            return null;
         }
-        result.setNpc(object.getJSONObject("zkb").getBoolean("npc"));
-        result.setTotalValue(object.getJSONObject("zkb").getLong("totalValue"));
-        result.setPoints(object.getJSONObject("zkb").getLong("points"));
-        result.setVictimId(object.getJSONObject("victim").getLong("characterID"));
-        result.setVictimAlliance(object.getJSONObject("victim").getString("allianceName"));
-        return result;
     }
 
     public static List<Killmail> parseKillmails(final JSONArray killmailArray) {
         List<Killmail> killmails = new ArrayList<>();
         for (int i = 0; i < killmailArray.length(); i++) {
             Killmail killmail = parseKillmail(killmailArray.getJSONObject(i));
-            killmails.add(killmail);
+            if (null != killmail) {
+                killmails.add(killmail);
+            }
         }
         return killmails;
     }
