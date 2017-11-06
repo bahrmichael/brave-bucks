f5d8328dc5b2faf1e42cc5c2d9620ffc9f5f849a
bahrmichael <mbahr92@gmail.com>
2017-11-06 15:11:34 +0100

    test fix

--- src/test/java/com/bravebucks/eve/web/rest/AdRequestResourceIntTest.java
+++ src/test/java/com/bravebucks/eve/web/rest/AdRequestResourceIntTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -36,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BraveBucksApp.class)
+@ContextConfiguration(initializers = EnvironmentTestConfiguration.class)
 public class AdRequestResourceIntTest {
 
     private static final String DEFAULT_REQUESTER = "AAAAAAAAAA";
